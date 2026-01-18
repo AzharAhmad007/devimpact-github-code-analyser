f11232164e0f087168a7704cfa79789e0294f3e2
Azhar Ahmad <97822308+AzharAhmad007@users.noreply.github.com>
2026-01-18 17:44:23 +0530

    Dev Impact GitHub contribution analysis platform implementation

---
+++ code-contribution-analyser/codeContributorProcessor/processor/constants.py
@@ -0,0 +1,3 @@
+GITHUB_MS_URL = "http://localhost:8081/graphql"
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+GEMINI_API_KEY = "AIzaSyBDVGmGxEypjPKbMMtnRaumnoWNV909E5M" 
